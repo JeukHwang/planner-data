b41d92198f15882fdb77692e9b1705e6cdebaa29
JeukHwang <jeukhwang.dev@gmail.com>
2022-11-16 12:07:48 +0900

    style: format with fixed prettier

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,7 @@ import { AE } from "./rule/AE";
 
 console.log("Index is executed!");
 // const user = new User([], new UserTrack("CS", ["EE"], ["ID"], ["CS"], false), 2022);
-const user = new User(
-  [],
-  new UserTrack("AE", ["EE"], ["ID"], ["CS"], false),
-  2022
-);
+const user = new User([], new UserTrack("AE", ["EE"], ["ID"], ["CS"], false), 2022);
 
 console.log(AE.validate(user));
 // console.log(AE.requirements.map(r => r.sentence).join("\n"));
